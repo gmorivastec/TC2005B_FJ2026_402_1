3f5cf02ca4cd4f9ff62ee9cbc108e1a1b0bf5077
Guillermo Rivas <gmo.rivas@gmail.com>
2026-02-23 20:54:00 -0600

    clase de feb 23

--- Assets/Movimiento.cs
+++ Assets/Movimiento.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class Movimiento : MonoBehaviour
 {
 
+    private InputSystem_Actions inputActions;
+
+    [SerializeField]
+    private float _velocidad = 5;
+
     // CÓDIGO!
 
     // ciclo de vida 
@@ -22,6 +27,18 @@ public class Movimiento : MonoBehaviour
     void Awake()
     {
         print("AWAKE");
+        inputActions = new InputSystem_Actions();
+    }
+
+    // más ciclo de vida!
+    void OnEnable()
+    {
+        inputActions.Enable();
+    }
+
+    void OnDisable()
+    {
+        inputActions.Disable();        
     }
 
     // segundo método en correr es start
@@ -29,7 +46,8 @@ public class Movimiento : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        print("START");
+        Debug.Log("START");
+        
     }
 
     // cómo funciona una app gráfica
@@ -45,10 +63,57 @@ public class Movimiento : MonoBehaviour
     {
         // invocado una vez por cuadro
 
-        print("UPDATE");
+        //print("UPDATE");
 
         //tratar de limitar código a 2 cuestiones en update:
         // 1. movimiento
         // 2. captar entrada
+
+        // VERIFICAR INPUT PARA BOTÓN
+        if(inputActions.Player.Jump.triggered)
+        {
+            // esto va a ser true sólo en una situación específica y sólo 1 frame a la vez
+            // frame anterior - desactivado, frame actual - activado
+            print("JUMP!");
+        }
+
+        // tipos genéricos
+        // mecanismo que permite parametrizar un tipo para un objeto o método
+        Vector2 desplazamiento = inputActions.Player.Move.ReadValue<Vector2>();
+        Debug.Log(desplazamiento);
+
+        // las entradas vector2 tienen rango [-1, 1]
+        // transform (t minúscula)
+        // objeto que heredo de MonoBehaviour que es una referencia
+        // al componente transform en este mismo GO
+
+        // PRIMERO 
+        // consistencia en movimiento entre diferentes desempeños
+
+        // Time.deltaTime - cantidad de tiempo en segundos que ha transcurrido entre
+        // el frame anterior y el actual
+        transform.Translate(desplazamiento * Time.deltaTime * _velocidad);
     }
+
+    void LateUpdate()
+    {
+        // sucede TODOS los frames después de todos los updates
+        // print("LATE UPDATE");
+    }
+
+    void FixedUpdate()
+    {
+        // corre (pretende correr) en intervalos de tiempo regulares
+        // print("FIXED UPDATE");
+    }
+
+    // detección de colisiones
+    // 1. motor de física
+    // 2. character controller
+
+    // 1. motor de física
+    // requisitos:
+    // - 2 o más objetos con colliders
+    // - al menos 1 objeto tiene el componente rigidbody 
+    // - el objeto con rigidbody se está moviendo (TODO: EXPLICAR POR QUÉ)
 }
